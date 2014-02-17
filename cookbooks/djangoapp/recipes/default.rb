eef7affe1f2369ff732d1a489ccd25d0f2d71bb9
Chris Cabral <chris.cabral@newbrandanalytics.com>
2014-02-17 13:43:49 -0500

    Use maaster djenkins recipe

--- cookbooks/djangoapp/recipes/default.rb
+++ cookbooks/djangoapp/recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'jenkins'
+include_recipe 'jenkins::master'
 include_recipe 'database'
 include_recipe 'mysql::server'
 include_recipe 'mysql::client'
